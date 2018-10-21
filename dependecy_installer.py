ce18b5d5380e3f1fe005368e29cb19b0e46d57f2
Piotr Pasterak <pasterak48@gmail.com>
2018-10-21 12:12:52 +0200

    automatic dependency installer

---
+++ dependecy_installer.py
@@ -0,0 +1,16 @@
+import subprocess
+
+if __name__ == '__main__':
+#kikvy part
+    subprocess.check_call(["python", '-m', 'pip', 'install', 'docutils'])
+    subprocess.check_call(["python", '-m', 'pip', 'install', 'pygments'])
+    subprocess.check_call(["python", '-m', 'pip', 'install', 'pypiwin32'])
+    subprocess.check_call(["python", '-m', 'pip', 'install', 'kivy.deps.glew'])
+    subprocess.check_call(["python", '-m', 'pip', 'install', 'kivy.deps.angle'])
+    subprocess.check_call(["python", '-m', 'pip', 'install', 'kivy'])
+#BeautifulSoup4 part
+    subprocess.check_call(["python", '-m', 'pip', 'install', 'requests'])
+    subprocess.check_call(["python", '-m', 'pip', 'install', 'BeautifulSoup4'])
+#Pony ORM
+    subprocess.check_call(["python", '-m', 'pip', 'install', 'pony'])
+
